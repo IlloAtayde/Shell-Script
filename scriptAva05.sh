26bef770a812beaa0cafa4e682949f1a0ca146f5
illoatayde <illoatayde@gmail.com>
2016-11-05 16:27:57 -0300

    Atualização scriptAva05.sh

--- scriptAva05.sh
+++ scriptAva05.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-#HISTFILE=~/.bash_history
-#set -o history
+HISTFILE=~/.bash_history
+set -o history
 #Comandos para ativar exeecução do comando history no script
-cat "$HOME/.bash_history"
+#cat "$HOME/.bash_history"
+usuario=suporte
+senha=suporte
+data=`date +"%y%m%d"`
+hora=`date +"%H%M"`
+history >> history.$USER.$data.$hora
+nomeArq=`ls | grep "history.$USER.$data.$hora"`
+ftp -n 192.168.2.2 << Fim &> ftp.mensagens.$data
+quote USER $usuario
+quote PASS $senha
+put $nomeArq
+quit
+Fim
